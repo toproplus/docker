6b5847263c3b921feb7f38f9093b7d686588012a
toproplus <40302335+toproplus@users.noreply.github.com>
2025-09-19 19:23:50 +0800

    修改hoppscoth

--- hoppscotch/conf/init.sh
+++ hoppscotch/conf/init.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 
+# 先修改好配置文件
+# cp /home/core/docker/hoppscotch/conf/.env .env
+
 # 首次启动后需要初始化数据库
 # 资料参考：https://docs.hoppscotch.io/documentation/self-host/community-edition/install-and-build#running-migrations
 
-# /usr/bin/docker run --rm --net nginx-network --env-file .env hoppscotch/hoppscotch:latest sh -c "pnpm dlx prisma migrate deploy"
-/usr/bin/docker exec hoppscotch pnpm dlx prisma migrate deploy
+/usr/bin/docker run --rm --net nginx-network --env-file .env hoppscotch/hoppscotch:latest sh -c "pnpm dlx prisma migrate deploy"
+# /usr/bin/docker exec hoppscotch pnpm dlx prisma migrate deploy
 
